3b04269eb234a042a3c963aa68f181af1c4be194
Dávid Szomolai <davidszomolai1@gmail.com>
2026-06-01 20:19:02 +0200

    pridanie header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header className="border-blue-700 border-2 rounded-md ">
+            <nav className="flex items-center justify-between">
+                <span className="text-white">Školský rozhlas SOŠTRV</span>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
