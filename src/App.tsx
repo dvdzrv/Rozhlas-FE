6c806f7afce2df1e64cb29f23b2ea08545a357c1
Dávid Szomolai <davidszomolai1@gmail.com>
2026-06-02 12:10:37 +0200

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,7 @@ import Playlist from "./components/Playlist.tsx";
 import Voting from "./components/Voting.tsx";
 import Footer from "./components/Footer.tsx";
 
-const API_URL = `${import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000'}`;
-
 function App() {
-    console.log(API_URL)
     return (
         <>
             <Header />
