ab75202887c8e2b8702db75d03d5ba1b86fe3d3a
Dávid Szomolai <davidszomolai1@gmail.com>
2026-06-02 13:07:32 +0200

    farba

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/hammerwerk_b.svg";
 function Header() {
     return (
         <div className="flex-1 p-5 mx-auto w-full">
-            <div className="text-center py-[30px] px-5 bg-linear-to-br from-[#E3C207] to-[#191414] rounded-[20px] mb-[30px] text-white shadow-[0_10px_30px_rgba(29,185,84,0.2)]">
+            <div className="text-center py-[30px] px-5 bg-linear-to-br from-[#E3C207] to-[#191414] rounded-[20px] mb-[30px] text-white shadow-[0_10px_30px_rgba(227, 194, 7, 1)]">
                 <button className="bg-[#191414] absolute top-10 left-10 text-white hover:text-[#E3C207] transition-colors duration-300 rounded-full p-2">
                     <a href="https://hammerwerk.sostrv.org">{"<"}</a>
                 </button>
